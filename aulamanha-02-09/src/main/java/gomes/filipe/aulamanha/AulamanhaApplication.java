23fdb34994d62ab47a473cb8696a341963a9dc60
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-09-02 16:11:21 -0300

    Adicionado aula da manhã e tarde

---
+++ aulamanha-02-09/src/main/java/gomes/filipe/aulamanha/AulamanhaApplication.java
@@ -0,0 +1,13 @@
+package gomes.filipe.aulamanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulamanhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulamanhaApplication.class, args);
+    }
+
+}
---
+++ aulamanha-02-09/src/test/java/gomes/filipe/aulamanha/AulamanhaApplicationTests.java
@@ -0,0 +1,13 @@
+package gomes.filipe.aulamanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulamanhaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
