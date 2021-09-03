96b2de00bdb3c7779a89ad1ce0db09aa373d143f
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-09-03 19:15:57 -0300

    Reinserido pasta de praticas

---
+++ praticas/praticaintegradora3/src/test/java/gomes/filipe/praticaintegradora3/Praticaintegradora3ApplicationTests.java
@@ -0,0 +1,13 @@
+package gomes.filipe.praticaintegradora3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praticaintegradora3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
