9bc10f47641e31152d7f807cb8d3e9f04a45ded3
Filipe Gomes <filipe.gomes@mercadolivre.com>
2021-09-20 16:07:47 -0300

    Adicionado aula tarde

--- aulas/desafio-spring/src/main/java/gomes/filipe/desafiospring/controller/NewPingController.java
+++ aulas/desafio-spring/src/main/java/gomes/filipe/desafiospring/controller/NewPingController.java
@@ -1,15 +1,69 @@
 package gomes.filipe.desafiospring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import gomes.filipe.desafiospring.model.Professor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping(value = "/home")
 public class NewPingController {
 
-    @GetMapping("/ping")
+    private Map<String, String> map = new HashMap<>();
+
+    public NewPingController () {
+        map.put("fiipe", "gomes");
+    }
+
+    @GetMapping(value = "/ping")
     public String palavra() {
         return "New Pong";
+    }
+
+    @GetMapping(value = "/pessoa")
+    public String nomeCompleto(String nome) {
+        if (nome.equalsIgnoreCase("filipe")) {
+            return "Filipe Gomes de Lima";
+        } else {
+            return "Não é o Filipe";
+        }
+    }
+
+    @GetMapping(value = "/pessoal")
+    public String end2(String nome, String cargo) {
+        if (nome.equalsIgnoreCase("filipe") && cargo.equalsIgnoreCase("professor")) {
+            return "professor: Filipe Gomes de Lima";
+        } else {
+            if (nome.equalsIgnoreCase("filipe")) {
+                return "aluno: Filipe Gomes de Lima";
+            }
+        }
+        return "Não é o Filipe";
+    }
+
+    @GetMapping(value = "/query")
+    public String consulta(@RequestParam("q") String primeiroNome) {
+        Optional<String> item = map.entrySet().stream()
+                .filter(e -> primeiroNome.equalsIgnoreCase(e.getKey()))
+                .map(Map.Entry::getKey)
+                .findFirst();
+        return item.get();
+    }
+
+    @PostMapping(value = "/cadastra")
+    public void cadastra(@RequestBody Professor payLoad) {
+        map.put(payLoad.getPrimeiroNome(), payLoad.getSegundoNome());
+    }
+
+    @PutMapping(value = "/atualiza")
+    public void atualizar(@RequestBody Professor payLoad) {
+        map.put(payLoad.getPrimeiroNome(), payLoad.getSegundoNome());
+    }
+
+    @DeleteMapping(value = "deleta/{i}")
+    public void remover(@PathVariable("i") String nome) {
+        map.remove(nome);
     }
 }
---
+++ aulas/desafio-spring/src/main/java/gomes/filipe/desafiospring/model/Professor.java
@@ -0,0 +1,28 @@
+package gomes.filipe.desafiospring.model;
+
+public class Professor {
+
+    private String primeiroNome;
+    private String segundoNome;
+
+    public Professor(String primeiroNome, String segundoNome) {
+        this.primeiroNome = primeiroNome;
+        this.segundoNome = segundoNome;
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSegundoNome() {
+        return segundoNome;
+    }
+
+    public void setSegundoNome(String segundoNome) {
+        this.segundoNome = segundoNome;
+    }
+}
